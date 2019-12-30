69194bab51c15f495181bb96a719084665b522ac
nm86nm <nm86nm@gmail.com>
2019-12-30 17:46:31 +0100

    Changing MainMethod

--- src/three/MainMethod.java
+++ src/three/MainMethod.java
@@ -1,8 +1,19 @@
 package three;
 
-public class Calculator {
-    public static void main(String[] args) {
-        new Thread(new Operations(5, 2)).start();
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class MainMethod {
+    public static void main(String[] args) {
+        /*for(int i=1; i<=5; i++) {
+            new Thread(new TextPrinter("Thread number: " + i)).start();
+        } test*/
+
+        ExecutorService executorService = Executors.newCachedThreadPool();
+
+        for(int i=1; i<=5; i++)
+            executorService.execute(new TextPrinter("Text number " + i));
+
+        executorService.shutdown();
     }
 }
